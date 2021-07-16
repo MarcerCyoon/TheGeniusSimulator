9f354bb397199d81855ae0e1e7b25b3d602a8c87
MarcerCyoon <4866635+MarcerCyoon@users.noreply.github.com>
2021-07-16 19:51:00 -0400

    added multi-tol support

--- static/globals.js
+++ static/globals.js
@@ -1,3 +1,45 @@
+class MainMatch {
+	constructor(name, episode, minTOL, maxTOL) {
+		this.name = name;
+		this.episode = episode;
+		this.minTOL = minTOL; // minTOLs, excluding ties.
+
+		if (maxTOL >= minTOL) {
+			this.maxTOL = maxTOL; // maxTOLs, excluding ties.
+		} else {
+			this.maxTOL = minTOL;
+		}
+
+	}
+
+	get TOLs() {
+		return this.genTOLs();
+	}
+
+	genTOLs() {
+		var num;
+
+		// If the max TOL is 1, account for the possibility of ties. Max ties of 3, for now.
+		if (this.maxTOL == 1) {
+			if (getRandomInt(0, 10) == 0) {
+				if (getRandomInt(0, 10) < 3) {
+					num = 3;
+				} else {
+					num = 2;
+				}
+			} else {
+				num = 1;
+			}
+		} else {
+			num = getRandomInt(this.minTOL, this.maxTOL + 1);
+		}
+
+		// Don't accidentally hand out more TOLs than players and make sure two always don't have TOLs
+		return Math.min(num, 12 - this.episode);
+	}
+
+}
+
 const dmArray = [
 	"Same Picture Hunt",
 	"Quattro",
@@ -22,50 +64,50 @@ const dmArray = [
 ];
 
 const mmArray = [
-	["123 Game", 1],
-	["Food Chain", 1],
-	["Fruit Stand", 1],
-	["ID Exchange", 1],
-	["Election Game", 2],
-	["Seat Exchange", 2],
-	["Jury Game", 2],
-	["Horror Race", 2],
-	["Abundance and Famine", 3],
-	["King Game", 3],
-	["Middle Race", 3],
-	["Today's Menu", 3],
-	["Zombie Game", 4],
-	["Blackout Game", 4],
-	["Swords and Shields", 4],
-	["Fish Shop", 4],
-	["Scamming Horse Race", 5],
-	["7 Commandments", 5],
-	["Miner Game", 5],
-	["Loyalists and Rebels", 5],
-	["Catch the Burglar", 6],
-	["Monopoly Game", 6],
-	["Stormy Stock Market", 6],
-	["Garnet Thief", 6],
-	["Open, Pass", 7],
-	["God's Judgment", 7],
-	["Constellation Game", 7],
-	["Seed Poker", 7],
-	["Kong's Dilemma", 8],
-	["Minus Auction", 8],
-	["Investment and Donation", 8],
-	["Minus Auction II", 8],
-	["Expression Auction", 9],
-	["Layoff", 9],
-	["Middle Race II", 9],
-	["Horror Race II", 9],
-	["Confined Scamming Horse Race", 10],
-	["Big Deal Game", 10],
-	["Chain Auction", 10],
-	["Cooperative Hold'em", 10],
-	["5:5 Game", 11],
-	["Elevator Game", 11],
-	["Doubting Yutnori", 11],
-	["How Much?", 11]
+	new MainMatch("123 Game", 1, 1, 1),
+	new MainMatch("Food Chain", 1, 1, 9),
+	new MainMatch("Fruit Stand", 1, 1, 1),
+	new MainMatch("ID Exchange", 1, 1, 1),
+	new MainMatch("Election Game", 2, 1, 1),
+	new MainMatch("Seat Exchange", 2, 5, 5),
+	new MainMatch("Jury Game", 2, 5, 7), // should only be 5 or 7 but currently no support.
+	new MainMatch("Horror Race", 2, 1, 1),
+	new MainMatch("Abundance and Famine", 3, 1, 1),
+	new MainMatch("King Game", 3, 1, 1),
+	new MainMatch("Middle Race", 3, 9, 9),
+	new MainMatch("Today's Menu", 3, 1, 1),
+	new MainMatch("Zombie Game", 4, 1, 8),
+	new MainMatch("Blackout Game", 4, 5, 5),
+	new MainMatch("Swords and Shields", 4, 5, 5),
+	new MainMatch("Fish Shop", 4, 1, 1),
+	new MainMatch("Scamming Horse Race", 5, 1, 1),
+	new MainMatch("7 Commandments", 5, 1, 1),
+	new MainMatch("Miner Game", 5, 1, 1),
+	new MainMatch("Loyalists and Rebels", 5, 1, 1),
+	new MainMatch("Catch the Burglar", 6, 3, 6),
+	new MainMatch("Monopoly Game", 6, 1, 1),
+	new MainMatch("Stormy Stock Market", 6, 1, 1),
+	new MainMatch("Garnet Thief", 6, 1, 1),
+	new MainMatch("Open, Pass", 7, 1, 1),
+	new MainMatch("God's Judgment", 7, 1, 1),
+	new MainMatch("Constellation Game", 7, 1, 1),
+	new MainMatch("Seed Poker", 7, 1, 1),
+	new MainMatch("Kong's Dilemma", 8, 1, 3),
+	new MainMatch("Minus Auction", 8, 1, 1),
+	new MainMatch("Investment and Donation", 8, 1, 1),
+	new MainMatch("Minus Auction II", 8, 1, 1),
+	new MainMatch("Expression Auction", 9, 1, 1),
+	new MainMatch("Layoff", 9, 1, 3),
+	new MainMatch("Middle Race II", 9, 1, 1),
+	new MainMatch("Horror Race II", 9, 1, 1),
+	new MainMatch("Confined Scamming Horse Race", 10, 2, 2),
+	new MainMatch("Big Deal Game", 10, 1, 1),
+	new MainMatch("Chain Auction", 10, 1, 1),
+	new MainMatch("Cooperative Hold'em", 10, 1, 1),
+	new MainMatch("5:5 Game", 11, 1, 1),
+	new MainMatch("Elevator Game", 11, 1, 1),
+	new MainMatch("Doubting Yutnori", 11, 1, 1),
+	new MainMatch("How Much?", 11, 1, 1)
 ];
 
 const playersArrays = [
